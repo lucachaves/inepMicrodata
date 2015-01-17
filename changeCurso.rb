25a421843c6d98df16bd8846f130a7d6cf48a74a
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2015-01-16 21:10:15 -0300

    initial commit

---
+++ changeCurso.rb
@@ -0,0 +1,28 @@
+require 'sequel'
+require 'progress_bar'
+
+db_inep = Sequel.connect('postgres://postgres:postgres@10.0.1.7/inep-curso')
+db_mc = Sequel.connect('postgres://postgres:postgres@10.0.1.7/mc-munic')
+
+# read all curso
+cursos = db_inep.from(:curso)
+munics = db_mc.from(:munic)
+
+result = cursos.all
+bar = ProgressBar.new(result.count)
+result.each{|curso|
+	id = curso[:id]
+	# insert nome ascii
+	
+	# get cidade and uf, then search latlon from mc-munic
+	munic = munics.where(nome: curso[:no_municipio_curso], uf: curso[:sgl_uf_curso])
+	if munic.count > 0
+		# insert latlon
+		cursos.
+			where(id: id).
+			update(latitude: munic.all[0][:latitude].to_f, longitude: munic.all[0][:longitude].to_f)
+	end
+
+	bar.increment!
+}
+
