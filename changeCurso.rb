c2eeb4f9f9a59f8421b0660c46914121a3a74039
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2015-01-16 22:15:10 -0300

    ascii city & todo sucupira

--- changeCurso.rb
+++ changeCurso.rb
@@ -12,16 +12,27 @@ result = cursos.all
 bar = ProgressBar.new(result.count)
 result.each{|curso|
 	id = curso[:id]
+
+	# insert sigla
+
 	# insert nome ascii
-	
-	# get cidade and uf, then search latlon from mc-munic
-	munic = munics.where(nome: curso[:no_municipio_curso], uf: curso[:sgl_uf_curso])
-	if munic.count > 0
-		# insert latlon
-		cursos.
+	city = curso[:no_municipio_curso]
+	city = city.tr(
+		"ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+		"AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
+	)
+	cursos.
 			where(id: id).
-			update(latitude: munic.all[0][:latitude].to_f, longitude: munic.all[0][:longitude].to_f)
-	end
+			update(no_municipio_curso_ascii: city.downcase)
+
+	# # get cidade and uf, then search latlon from mc-munic
+	# munic = munics.where(nome: curso[:no_municipio_curso], uf: curso[:sgl_uf_curso])
+	# if munic.count > 0
+	# 	# insert latlon
+	# 	cursos.
+	# 		where(id: id).
+	# 		update(latitude: munic.all[0][:latitude].to_f, longitude: munic.all[0][:longitude].to_f)
+	# end
 
 	bar.increment!
 }
---
+++ sucupiraDump.rb
@@ -0,0 +1,3 @@
+# Fazer crawler do sucupira de todas as IES e Programas
+# No futuro analisar ano e linha de pesquisa
+# https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/dadosCadastrais/dadosCadastraisPublico.jsf
